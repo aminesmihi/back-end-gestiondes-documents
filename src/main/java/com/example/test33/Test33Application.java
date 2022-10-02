cc11b2824375689cdaa13a4eb392d1bee8ca978b
amine smihi <you@example.com>
2022-10-02 20:37:09 +0200

    ajouter projet back-end

---
+++ src/main/java/com/example/test33/Test33Application.java
@@ -0,0 +1,13 @@
+package com.example.test33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test33Application.class, args);
+	}
+
+}
